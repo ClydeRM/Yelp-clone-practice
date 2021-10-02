cec99c4c248125bb8f90cb91ceddd62293a0dcd2
ClydeRM <clyde.official@icloud.com>
2021-10-02 21:35:42 +0800

    Route 3 page use react-route-dom

--- cliant/src/App.jsx
+++ cliant/src/App.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { RestaurantsContextProvider } from "./context/RestaurantsContext";
+import Home from "./routes/Home";
+import RestaurantDetailPage from "./routes/RestaurantDetailPage";
+import UpdatePage from "./routes/UpdatePage";
 
 const App = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <RestaurantsContextProvider>
+      <div className="container">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route
+              exact
+              path="/restaurants/:id"
+              component={RestaurantDetailPage}
+            />
+            <Route
+              exact
+              path="/restaurants/:id/update"
+              component={UpdatePage}
+            />
+          </Switch>
+        </Router>
+      </div>
+    </RestaurantsContextProvider>
+  );
+};
 
 export default App;
