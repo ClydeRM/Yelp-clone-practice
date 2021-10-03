b26de83939c15ad9b16c31a163935f4449ea084d
ClydeRM <clyde.official@icloud.com>
2021-10-03 17:01:21 +0800

    add auto update useState func addRestaurants

--- cliant/src/context/RestaurantsContext.js
+++ cliant/src/context/RestaurantsContext.js
@@ -4,9 +4,11 @@ export const RestaurantsContext = createContext();
 
 export const RestaurantsContextProvider = (props) => {
   const [restaurants, setRestaurants] = useState([]);
-
+  const addRestaurants = (restaurant) => { // auto add new data in context array to render new data
+    setRestaurants([...restaurants, restaurant]);
+  }
   return (
-    <RestaurantsContext.Provider value={{ restaurants, setRestaurants }}>
+    <RestaurantsContext.Provider value={{ restaurants, setRestaurants, addRestaurants}}>
       {props.children}
     </RestaurantsContext.Provider>
   );
