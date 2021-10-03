95b57182f73f688fbd5609201c4ba58a5918e0f0
ClydeRM <clyde.official@icloud.com>
2021-10-03 17:08:59 +0800

    setup delete and update btn render without reload

--- cliant/src/components/RestaurantList.jsx
+++ cliant/src/components/RestaurantList.jsx
@@ -1,46 +1,97 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import RestaurantFinder from "../apis/RestaurantFinder";
+import { RestaurantsContext } from "../context/RestaurantsContext";
 
-
-const RestaurantList = () => {
-
-    useEffect(() =>{
-      const getRestaurants = async () => {
-        try {
-          const response = await RestaurantFinder.get("/");
-          console.log(response);
-        } catch (error) {
-          console.error(error);
-        }
+const RestaurantList = (props) => {
+  const { restaurants, setRestaurants } = useContext(RestaurantsContext);
+  let history = useHistory(); // for route to update Page
+  useEffect(() => {
+    const getRestaurants = async () => {
+      try {
+        const response = await RestaurantFinder.get("/");
+        // console.log(response);
+        setRestaurants(response.data.data.restaurants);
+      } catch (error) {
+        console.error(error);
       }
-      getRestaurants();
-    },[]); // 3.25.00
-    return (
-      <div className="list-group">
-        <table className="table table-dark table-hover">
-          <thead>
-            <tr className="bg-primary">
-              <th scope="col">Restaurant</th>
-              <th scope="col">Location</th>
-              <th scope="col">Price Range</th>
-              <th scope="col">Rating</th>
-              <th scope="col">Edit</th>
-              <th scope="col">Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>mc</td>
-              <td>mc</td>
-              <td>mc</td>
-              <td>mc</td>
-              <td><button className="btn btn-warning">Update</button></td>
-              <td><button className="btn btn-danger">Delete</button></td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    );
-}
+    };
+    getRestaurants();
+  }, []); // 3.25.00
+
+  const handleDelete = async (id) => {
+    try {
+      const response = await RestaurantFinder.delete(`/${id}`);
+      // console.log(response);
+      setRestaurants(
+        restaurants.filter((restaurant) => {
+          return restaurant.id !== id;
+        })
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleUpdate = async (id) => {
+    history.push(`/restaurants/${id}/update`);
+  };
+
+  return (
+    <div className="list-group">
+      <table className="table table-dark table-hover">
+        <thead>
+          <tr className="bg-primary">
+            <th scope="col">Restaurant</th>
+            <th scope="col">Location</th>
+            <th scope="col">Price Range</th>
+            <th scope="col">Rating</th>
+            <th scope="col">Edit</th>
+            <th scope="col">Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {restaurants &&
+            restaurants.map((restaurant) => {
+              return (
+                <tr key={restaurant.id}>
+                  <td>{restaurant.name}</td>
+                  <td>{restaurant.location}</td>
+                  <td>{"$".repeat(restaurant.price_range)}</td>
+                  <td>review</td>
+                  <td>
+                    <button
+                      onClick={() => handleUpdate(restaurant.id)}
+                      className="btn btn-warning"
+                    >
+                      Update
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      onClick={() => handleDelete(restaurant.id)}
+                      className="btn btn-danger"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+        </tbody>
+        {/* <tbody>
+          <tr>
+            <td>mc</td>
+            <td>mc</td>
+            <td>mc</td>
+            <td>mc</td>
+            <td><button className="btn btn-warning">Update</button></td>
+            <td><button className="btn btn-danger">Delete</button></td>
+          </tr>
+        </tbody> */}
+      </table>
+    </div>
+  );
+};
 
 export default RestaurantList;
