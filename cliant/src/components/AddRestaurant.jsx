447b98cb4eb3a703f0d14849febd3c1741d19108
ClydeRM <clyde.official@icloud.com>
2021-10-03 17:06:19 +0800

    setup add btn and render new data without reload

--- cliant/src/components/AddRestaurant.jsx
+++ cliant/src/components/AddRestaurant.jsx
@@ -1,22 +1,55 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import RestaurantFinder from "../apis/RestaurantFinder";
+import { RestaurantsContext } from "../context/RestaurantsContext";
 
 const AddRestaurant = () => {
+  const { addRestaurants } = useContext(RestaurantsContext);
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
+  const [priceRange, setPriceRange] = useState("Price Range");
+  const handleSubmit = async (e) =>{
+    e.preventDefault(); // prevent page to reload
+    try {
+      const response = await RestaurantFinder.post("/", {
+        name: name,
+        location: location,
+        price_range: priceRange,
+      });
+      addRestaurants(response.data.data.restaurants); // auto add new data to context let app to render but doesn't reload
+      // console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="mb-4">
       <form action="">
         <div className="form-row">
           <div className="col">
-            <input type="text" className="form-control" placeholder="Name" />
+            <input
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              type="text"
+              className="form-control"
+              placeholder="Name"
+            />
           </div>
           <div className="col">
             <input
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
               type="text"
               className="form-control"
               placeholder="Location"
             />
           </div>
           <div className="col">
-            <select className="custom-select my-1 mr-sm-2">
+            <select
+              value={priceRange}
+              onChange={(e) => setPriceRange(e.target.value)}
+              className="custom-select my-1 mr-sm-2"
+            >
               <option disabled>Price Range</option>
               <option value="1">$</option>
               <option value="2">$$</option>
@@ -25,7 +58,7 @@ const AddRestaurant = () => {
               <option value="5">$$$$$</option>
             </select>
           </div>
-          <button className="btn btn-primary">Add</button>
+          <button type="submit" onClick={handleSubmit} className="btn btn-primary">Add</button>
         </div>
       </form>
     </div>
