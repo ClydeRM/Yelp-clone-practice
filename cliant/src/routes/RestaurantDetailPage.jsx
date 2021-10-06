f97de82b76d0acf462451edaa4d87e4af4af371d
ClydeRM <clyde.official@icloud.com>
2021-10-06 15:52:22 +0800

    fetch restaurant data by params id

--- cliant/src/routes/RestaurantDetailPage.jsx
+++ cliant/src/routes/RestaurantDetailPage.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { RestaurantsContext } from "../context/RestaurantsContext";
+import RestaurantFinder from "../apis/RestaurantFinder";
 
 const RestaurantDetailPage = () => {
-    return (
-        <div>
-            Detail page
-        </div>
-    )
-}
+  const { id } = useParams();
+  const { selectRestaurant, setSelectRestaurant } =
+    useContext(RestaurantsContext);
+
+  useEffect(() => {
+    const getRestaurant = async () => {
+      try {
+        const response = await RestaurantFinder.get(`/${id}`);
+        // console.log(response);
+        setSelectRestaurant(response.data.data.restaurant);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getRestaurant();
+  }, []);
+
+  return <div>{selectRestaurant && selectRestaurant.name }</div>;
+};
 
 export default RestaurantDetailPage;
