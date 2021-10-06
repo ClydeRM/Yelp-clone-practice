c9041ea65de6c1fb482f714e4d936f3632e87f2c
ClydeRM <clyde.official@icloud.com>
2021-10-06 15:55:21 +0800

    Add select restaurant by state

--- cliant/src/context/RestaurantsContext.js
+++ cliant/src/context/RestaurantsContext.js
@@ -4,11 +4,22 @@ export const RestaurantsContext = createContext();
 
 export const RestaurantsContextProvider = (props) => {
   const [restaurants, setRestaurants] = useState([]);
-  const addRestaurants = (restaurant) => { // auto add new data in context array to render new data
+  const addRestaurants = (restaurant) => {
+    // auto add new data in context array to render new data
     setRestaurants([...restaurants, restaurant]);
-  }
+  };
+  const [selectRestaurant, setSelectRestaurant] = useState(null);
+
   return (
-    <RestaurantsContext.Provider value={{ restaurants, setRestaurants, addRestaurants}}>
+    <RestaurantsContext.Provider
+      value={{
+        restaurants,
+        setRestaurants,
+        addRestaurants,
+        selectRestaurant,
+        setSelectRestaurant,
+      }}
+    >
       {props.children}
     </RestaurantsContext.Provider>
   );
