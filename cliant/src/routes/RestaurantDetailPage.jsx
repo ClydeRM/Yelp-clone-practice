b0695aee03697a7dca0dc985903210b3afb634a0
ClydeRM <clyde.official@icloud.com>
2021-10-10 21:19:31 +0800

    add components and fetch data to keep page fresh

--- cliant/src/routes/RestaurantDetailPage.jsx
+++ cliant/src/routes/RestaurantDetailPage.jsx
@@ -2,6 +2,9 @@ import React, { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { RestaurantsContext } from "../context/RestaurantsContext";
 import RestaurantFinder from "../apis/RestaurantFinder";
+import StartRating from "../components/StartRating";
+import Reviews from "../components/Reviews";
+import AddReview from "../components/AddReview";
 
 const RestaurantDetailPage = () => {
   const { id } = useParams();
@@ -9,19 +12,41 @@ const RestaurantDetailPage = () => {
     useContext(RestaurantsContext);
 
   useEffect(() => {
-    const getRestaurant = async () => {
+    const fetchData = async () => {
       try {
         const response = await RestaurantFinder.get(`/${id}`);
         // console.log(response);
-        setSelectRestaurant(response.data.data.restaurant);
+        setSelectRestaurant(response.data.data);
       } catch (error) {
         console.error(error);
       }
     };
-    getRestaurant();
+    fetchData();
   }, []);
 
-  return <div>{selectRestaurant && selectRestaurant.name }</div>;
+  return (
+    <div>
+      {selectRestaurant && (
+        <>
+          <h1 className="text-center display-1">
+            {selectRestaurant.restaurant.name}
+          </h1>
+          <div className="text-center">
+            <StartRating rating={selectRestaurant.restaurant.average_rating} />
+            <span className="text-warning ml-1">
+              {selectRestaurant.restaurant.count
+                ? `(${selectRestaurant.restaurant.count})`
+                : "(0)"}
+            </span>
+          </div>
+          <div className="mt-3">
+            <Reviews reviews={selectRestaurant.reviews} />
+          </div>
+          <AddReview />
+        </>
+      )}
+    </div>
+  );
 };
 
 export default RestaurantDetailPage;
